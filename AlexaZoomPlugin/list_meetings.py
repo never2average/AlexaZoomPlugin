ceb13645197dfd011677da17d638d7aade531687
never2average <priyesh.sriv2017@gmail.com>
2020-05-07 23:04:07 +0530

    List Today's Meetings

--- AlexaZoomPlugin/list_meetings.py
+++ AlexaZoomPlugin/list_meetings.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from dateutil.parser import parse
 
 
@@ -20,10 +20,9 @@ def listmeetings(pageno=1, userid="foo@bar.com", jwt=""):
     data = response.json()
     today_s_meetings = []
     for i in data["meetings"]:
-        if (
-            parse(i["created_at"]) >= datetime.now() and 
-            parse(i["created_at"]) <= datetime.now()+timedelta(days=1)
-        ):
+        current_time = datetime.now(timezone.utc)
+        meeting_time = parse(i["created_at"])
+        if meeting_time >= current_time and meeting_time <= current_time+timedelta(hours=24):
             today_s_meetings.append(i) 
     data["meetings"] = today_s_meetings
     data["statusCode"] = response.status_code
